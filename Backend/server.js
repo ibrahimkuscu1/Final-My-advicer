6d9dc8930a4f2e6b7358a9917813dbdbef41bf2e
ibrahim kuscu <kuscu9834@gmail.com>
2022-10-09 13:49:05 +0300

    database and mongodb connection, first schema

---
+++ Backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const connection= require("./database/connection")
+const user = require("./database/models/user")
+const { model } = require('mongoose');
+const port=5000
+
+// middleware
+app.use(express.json());
+// app.use(express.urlencoded({extended:false}))
+
+
+// routes
+const API=require("./routers/api")
+
+app.use("/",API)
+
+
+
+
+app.get('/', (req, res) => {
+    res.send("hello world hey!!!")
+})
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
